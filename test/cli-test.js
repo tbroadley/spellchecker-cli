035e52dac95181480100ffd11004ddec38c025c0
Thomas Broadley <buriedunderbooks@hotmail.com>
2018-04-24 09:09:22 -0400

    Treat dictionaries as lists of regexes

--- test/cli-test.js
+++ test/cli-test.js
@@ -283,4 +283,21 @@ parallel('Spellchecker CLI', function testSpellcheckerCLI() {
     const result = await runWithArguments('test/fixtures/incorrect-2.txt --dictionaries test/fixtures/dictionaries/programmatic.js test/fixtures/dictionaries/two.txt');
     result.should.not.have.property('code');
   });
+
+  it('treats dictionary entries as regexes', async () => {
+    const result = await runWithArguments('test/fixtures/incorrect.txt --dictionaries test/fixtures/dictionaries/regex.txt');
+    result.should.not.have.property('code');
+  });
+
+  it('treats dictionary entries as if they were wrapped in ^ and $', async () => {
+    const { code, stdout } = await runWithArguments('test/fixtures/incorrect.txt --dictionaries test/fixtures/dictionaries/regex-no-match.txt');
+    code.should.equal(1);
+    stdout.should.include('`Thisisnotaword` is misspelt');
+    stdout.should.include('`preprocessed` is misspelt');
+  });
+
+  it('supports programmatic dictionaries that pass mixed regexes and strings', async () => {
+    const result = await runWithArguments('test/fixtures/incorrect.txt --dictionaries test/fixtures/dictionaries/regex.js');
+    result.should.not.have.property('code');
+  });
 });
