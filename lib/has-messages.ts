230446397244181931782f040fb2d88119ffb029
Thomas Broadley <buriedunderbooks@hotmail.com>
2021-06-13 17:30:02 -0400

    Remove more any usages

--- lib/has-messages.ts
+++ lib/has-messages.ts
@@ -1,7 +1,11 @@
 import constant from 'lodash/constant';
 import sumBy from 'lodash/sumBy';
+import { VFile, VFileMessage } from 'vfile-reporter';
 
-export const hasMessages = (vfiles: any, filterMessages: any = constant(true)) => {
-  const messageCount = sumBy(vfiles, (file: any) => file.messages.filter(filterMessages).length);
+export const hasMessages = (
+  vfiles: VFile[],
+  filterMessages: (message: VFileMessage) => boolean = constant(true)
+): boolean => {
+  const messageCount = sumBy(vfiles, file => file.messages.filter(filterMessages).length);
   return messageCount > 0;
 };
