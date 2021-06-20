2bc20ffb6278bda4c727549657742937f53af7df
gadhagod <69025547+gadhagod@users.noreply.github.com>
2021-06-19 18:15:54 -0700

    Print all errors in package.json configuration

--- lib/config/package.ts
+++ lib/config/package.ts
@@ -1,19 +1,28 @@
-import { readFileSync } from 'fs';
+import { accessSync, readFileSync } from 'fs';
+
+import appRootPath from 'app-root-path';
 
 import { printError } from '../print-error';
 
 import { ExternalConfig } from './types';
 
 export function readFromPackage(): ExternalConfig {
+  const path = appRootPath.resolve('package.json');
+  try {
+    accessSync(path);
+  } catch {
+    return {};
+  }
   let config;
   try {
-    config = JSON.parse(readFileSync('package.json') as unknown as string);
+    config = JSON.parse(readFileSync(path, 'utf-8'));
   } catch (err) {
     if ((err as Error).name === 'SyntaxError') {
       printError('Unable to parse package.json');
-      process.exit(1);
+    } else {
+      printError(`Unable to parse package.json: ${(err as Error).message}`);
     }
-    return {};
+    process.exit(1);
   }
   if ('spellchecker' in config) {
     return config.spellchecker;
